34791cf9d51af56e8c66da1cf558d0ba09a6031a
Loh Chin Yee <percussionist8896@gmail.com>
2025-02-20 15:36:27 +0800

    add locatorhandler for popup modal

--- tests/test-login.spec.ts
+++ tests/test-login.spec.ts
@@ -7,9 +7,19 @@ const mail = process.env.USER_EMAIL
 const password = process.env.USER_PASSWORD
 
 test('login through homepage', async({page}) => {
+    await page.addLocatorHandler(page.getByRole('dialog'), async () => {
+        // if there is a dialog popup, click somewhere to close
+        await homepage_account.accountBtn.click()
+    })
+
+    await page.addLocatorHandler(page.locator(".dy-modal-container"), async () => {
+        await homepage_account.accountBtn.click()
+    })
+
     const homepage_account = new CastleryHomePage_AccountSection(page)
 
     await homepage_account.goto()
+
     await homepage_account.login(mail, password)
 
     await homepage_account.hasValidAccountMenuList()
